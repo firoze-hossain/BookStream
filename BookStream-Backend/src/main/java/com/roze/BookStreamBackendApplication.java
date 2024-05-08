a3f42605298e9e47464097e510143bda61bcbbd7
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-05-08 16:48:53 +0600

    backend project initialized with all dependencies

---
+++ BookStream-Backend/src/main/java/com/roze/BookStreamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.roze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStreamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStreamBackendApplication.class, args);
+    }
+
+}
