1be2918365f9a4c806af1a7cd47c370ea1d5794e
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-05-26 09:56:22 +0600

    implementation done my book page

--- BookStream-Frontend/src/app/modules/book/components/book-card/book-card.component.ts
+++ BookStream-Frontend/src/app/modules/book/components/book-card/book-card.component.ts
@@ -36,6 +36,7 @@ export class BookCardComponent {
     return this._manage;
   }
 
+  @Input()
   set manage(value: boolean) {
     this._manage = value;
   }
---
+++ BookStream-Frontend/src/app/modules/book/pages/my-books/my-books.component.ts
@@ -0,0 +1,77 @@
+import {Component, OnInit} from '@angular/core';
+import {PageResponseBookResponse} from "../../../../services/models/page-response-book-response";
+import {BookService} from "../../../../services/services/book.service";
+import {Router} from "@angular/router";
+import {BookResponse} from "../../../../services/models/book-response";
+
+@Component({
+  selector: 'app-my-books',
+  templateUrl: './my-books.component.html',
+  styleUrls: ['./my-books.component.scss']
+})
+export class MyBooksComponent implements OnInit{
+  bookResponse: PageResponseBookResponse = {};
+  page = 0;
+  size = 5;
+
+  constructor(private bookService: BookService,
+              private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.findAllBooks();
+  }
+
+  private findAllBooks() {
+    this.bookService.findAllBooksByUser({
+      page: this.page,
+      size: this.size
+    }).subscribe({
+      next: (books) => {
+        this.bookResponse = books;
+      }
+    })
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllBooks();
+  }
+
+  goToPreviousPage() {
+    this.page--;
+    this.findAllBooks();
+  }
+
+  goToPage(page: number) {
+    this.page = page;
+    this.findAllBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.bookResponse.totalPages as number - 1;
+    this.findAllBooks();
+  }
+
+  get isLastPage(): boolean {
+    return this.page == this.bookResponse.totalPages as number - 1;
+
+  }
+
+  archiveBook(book: BookResponse) {
+
+  }
+
+  shareBook(book: BookResponse) {
+
+  }
+
+  editBook(book: BookResponse) {
+
+  }
+}
